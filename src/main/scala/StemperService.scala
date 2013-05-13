6aa6fc6961d95ca9a7f144bb0f0a55be9debfd9f
alippai <adam@rigo.sk>
2013-05-14 01:54:15 +0200

    Some stability fixes.

--- src/main/scala/StemperService.scala
+++ src/main/scala/StemperService.scala
@@ -16,14 +16,18 @@ import java.net.URL
 class StemperService extends Actor with SprayActorLogging {
   implicit val timeout: Timeout = 2.second // for the actor 'asks'
   import context.dispatcher // ExecutionContext for the futures and scheduler
+  import Uri._
+  import Uri.Path._
 
   def fastPath: Http.FastPath = {
-    case HttpRequest(GET, Uri.Path("/fast-ping"), _, _, _) => HttpResponse(entity = "FAST-PONG!")
-    case HttpRequest(GET, Uri.Path("/fast-html"), _, _, _) => index()
+    case HttpRequest(GET, Uri(_, _, Slash(Segment("fast-ping", Path.Empty)), _, _), _, _, _) =>
+      HttpResponse(entity = "FAST-PONG!")
   }
 
   def receive = {
     // when a new connection comes in we register ourselves as the connection handler
+    //case _: Http.Connected => sender ! Http.Register(self, fastPath = fastPath)
+
     case _: Http.Connected => sender ! Http.Register(self, fastPath = fastPath)
 
     case HttpRequest(GET, Uri.Path("/ping"), _, _, _) =>
@@ -45,7 +49,7 @@ class StemperService extends Actor with SprayActorLogging {
   //val source = scala.io.Source.fromURL(new URL("http://ustream.tv/")).mkString
 
   val myURL = getClass.getClassLoader.getResource("file.html")
-  val source = scala.io.Source.fromURL(myURL).mkString
+  val source = scala.io.Source.fromURL(myURL)("UTF-8").mkString
   val template = new Mustache(source)
 
   def index() = HttpResponse(
